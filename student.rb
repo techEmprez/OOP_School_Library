04bfead0c4e390ac6aad7d1a81d88c92939578d3
techEmprez <choiceosobor@gmail.com>
2022-11-05 17:43:45 +0100

    link classroom to student

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom)
-    super(age, name, parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     @classroom = classroom
+    super(age, name, parent_permission: parent_permission)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
