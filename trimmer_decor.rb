0784c979d277153912135b141d966287347d59ab
techEmprez <choiceosobor@gmail.com>
2022-11-05 16:40:32 +0100

    modified returning number of char

--- trimmer_decor.rb
+++ trimmer_decor.rb
@@ -1,6 +1,6 @@
 require_relative 'base_decor'
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    @nameable.correct_name.strip[0...11]
+    @nameable.correct_name.strip[0...10]
   end
 end
