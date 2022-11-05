e9d2212385f5b474e644fd85cedd7b53b4bb0f26
techEmprez <choiceosobor@gmail.com>
2022-11-05 14:13:16 +0100

    create variables and parameters for students

--- student.rb
+++ student.rb
@@ -1,1 +1,12 @@
 require './person'
+
+class Student < Person
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: true)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
