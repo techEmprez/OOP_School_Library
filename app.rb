0890fe4ee7eb7831ff096cccd06e1afb9d49fa35
techEmprez <choiceosobor@gmail.com>
2022-11-05 18:09:58 +0100

    create class App

--- app.rb
+++ app.rb
@@ -2,3 +2,10 @@ require_relative './book'
 require_relative './rental'
 require_relative './student'
 require_relative './teacher'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
